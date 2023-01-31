63794455262f624f76a60a7b6b47c4161c9c9ce4
Turuu30 <trmnhtr@gmail.com>
2023-01-31 10:34:28 +0800

    animation done

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ import {AppRegistry} from 'react-native';
 // import App from './src/App';
 import MoonSun from './src/MoonSun';
 import {name as appName} from './app.json';
-import Todo from './src/Todo';
-
+// import Todo from './src/Todo';
 
 AppRegistry.registerComponent(appName, () => MoonSun);
--- src/MoonSun.tsx
+++ src/MoonSun.tsx
@@ -10,53 +10,61 @@ import {
 
 const MoonSun = () => {
   const [isEnabled, setIsEnabled] = useState(false);
-  const right = useRef(new Animated.Value(0)).current;
-  const left = useRef(new Animated.Value(0)).current;
-  const opacity = useRef(new Animated.Value(1)).current;
-  const duration = 1500;
+  const opacity = useRef(new Animated.ValueXY({x: 1, y: 0})).current;
+  const btnMoon = useRef(new Animated.Value(0)).current;
+  const sunMove = useRef(new Animated.ValueXY()).current;
+  const moonMove = useRef(new Animated.ValueXY()).current;
+  const duration = 1000;
 
   const toggleSwitch = () => {
+    // ternary operator
     isEnabled
-      ? Animated.timing(right, {
-          toValue: 0,
-          duration,
-          useNativeDriver: true,
-        }).start()
-      : Animated.timing(right, {
-          toValue: -400,
+      ? (Animated.timing(opacity, {
+          toValue: {x: 1, y: 0},
           duration,
           useNativeDriver: true,
         }).start(),
-      // night
-      isEnabled
-        ? Animated.timing(left, {
-            toValue: 0,
-            duration,
-            useNativeDriver: true,
-          }).start()
-        : Animated.timing(left, {
-            toValue: 0,
-            duration,
-            useNativeDriver: true,
-          }).start(),
-      // opacity
-      isEnabled
-        ? Animated.timing(opacity, {
-            toValue: 1,
-            duration,
-            delay: 10,
-            useNativeDriver: true,
-          }).start()
-        : Animated.timing(opacity, {
-            toValue: 0,
-            duration,
-            useNativeDriver: true,
-          }).start(),
-      setIsEnabled(previousState => !previousState);
+        Animated.timing(btnMoon, {
+          toValue: 0,
+          duration: 10,
+          useNativeDriver: true,
+        }).start(),
+        Animated.timing(sunMove, {
+          toValue: 0,
+          duration: 1500,
+          useNativeDriver: true,
+        }).start(),
+        Animated.timing(moonMove, {
+          toValue: 0,
+          duration: 1200,
+          useNativeDriver: true,
+        }).start())
+      : // or
+        (Animated.timing(opacity, {
+          toValue: {x: 0, y: 1},
+          duration,
+          useNativeDriver: true,
+        }).start(),
+        Animated.timing(btnMoon, {
+          toValue: 1,
+          duration: 10,
+          useNativeDriver: true,
+        }).start(),
+        Animated.timing(sunMove, {
+          toValue: {x: -90, y: 170},
+          duration: 1500,
+          useNativeDriver: true,
+        }).start(),
+        Animated.timing(moonMove, {
+          toValue: {x: -200, y: 300},
+          duration: 2000,
+          useNativeDriver: true,
+        }).start());
+    setIsEnabled(previousState => !previousState);
   };
   return (
     <ScrollView style={styles.container}>
-      {/* <Animated.View style={{}}>
+      <Animated.View>
         <Image
           style={styles.night}
           source={{
@@ -64,8 +72,7 @@ const MoonSun = () => {
           }}
         />
       </Animated.View>
-      <Animated.View
-        style={{transform: [{translateX: right}], opacity: opacity}}>
+      <Animated.View style={{opacity: opacity.x}}>
         <Image
           style={styles.day}
           source={{
@@ -73,16 +80,46 @@ const MoonSun = () => {
           }}
         />
       </Animated.View>
-
+      <Animated.Image
+        style={[
+          styles.sun,
+          {
+            transform: [{translateY: sunMove.y}, {translateX: sunMove.x}],
+            opacity: opacity.x,
+          },
+        ]}
+        source={{
+          uri: 'https://cdn.discordapp.com/attachments/940059401333645348/1069458522812723270/Vector.png',
+        }}
+      />
+      <Animated.Image
+        style={[
+          styles.moon,
+          {
+            transform: [{translateY: moonMove.y}, {translateX: moonMove.x}],
+            // opacity: opacity.y,
+          },
+        ]}
+        source={{
+          uri: 'https://cdn.discordapp.com/attachments/940059401333645348/1069458694267473960/Vector_1.png',
+        }}
+      />
+      {/* <Animated.Image
+        style={[styles.star, {}]}
+        source={{
+          uri: 'https://cdn.discordapp.com/attachments/940059401333645348/1069458751926567002/Vector_2.png',
+        }}
+      /> */}
       <View style={styles.switch}>
         <Switch
-          trackColor={{false: '#767577', true: '#81b0ff'}}
-          thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
-          ios_backgroundColor="#3e3e3e"
+          trackColor={{false: '#27173A', true: '#27173A'}}
+          thumbColor="#FFC207"
+          ios_backgroundColor="#27173A"
           onValueChange={toggleSwitch}
           value={isEnabled}
         />
-      </View> */}
+        <Animated.View style={[styles.switchMoon, {opacity: btnMoon}]} />
+      </View>
     </ScrollView>
   );
 };
@@ -107,6 +144,36 @@ const styles = StyleSheet.create({
     position: 'absolute',
     right: 10,
     top: 50,
+  },
+  switchMoon: {
+    width: 20,
+    height: 20,
+    top: 2,
+    right: 1,
+    borderRadius: 50,
+    backgroundColor: '#27173A',
+    position: 'absolute',
+  },
+  sun: {
+    position: 'absolute',
+    top: 350,
+    left: 100,
+    width: 100,
+    height: 100,
+  },
+  moon: {
+    position: 'absolute',
+    top: -95,
+    right: 20,
+    width: 80,
+    height: 95,
+  },
+  star: {
+    position: 'absolute',
+    top: 150,
+    left: 100,
+    width: 120,
+    height: 95,
   },
 });
 
